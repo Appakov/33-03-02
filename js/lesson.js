98f6c02758ecbbf32dda2344a315a379ce6645e6
Appakov <147263500+Appakov@users.noreply.github.com>
2024-01-16 22:44:36 -1200

    two commit

--- js/lesson.js
+++ js/lesson.js
@@ -27,3 +27,45 @@ phoneButton.onclick = () => {
 // }
 // increment()
 
+const tabContents = document.querySelectorAll('.tab_content_block');
+const tabItems = document.querySelectorAll('.tab_content_item');
+const tabParents = document.querySelector('.tab_content_items');
+
+let currentTabIndex = 0; // начинаем с первой вкладки
+
+const hideTabContent = () => {
+    tabContents.forEach((tabBlock) => {
+        tabBlock.style.display = 'none';
+    });
+    tabItems.forEach((tab) => {
+        tab.classList.remove('tab_content_item_active');
+    });
+};
+
+const showTabContent = (index) => {
+    hideTabContent();
+    tabContents[index].style.display = "block";
+    tabItems[index].classList.add('tab_content_item_active');
+};
+
+const switchTabAutomatically = () => {
+    currentTabIndex = (currentTabIndex + 1) % tabItems.length;
+    showTabContent(currentTabIndex);
+    setTimeout(switchTabAutomatically, 3000);
+};
+
+hideTabContent();
+showTabContent(currentTabIndex);
+switchTabAutomatically();
+
+tabParents.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabItems.forEach((tabItem, tabIndex) => {
+            if (event.target === tabItem) {
+                currentTabIndex = tabIndex; // установка индекса выбранной вкладки
+                hideTabContent();
+                showTabContent(currentTabIndex);
+            }
+        });
+    }
+};
